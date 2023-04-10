3f98222f296c70fccbdaed918d0cc88965ce9eb3
lewisky <lewisky28mawira@gmail.com>
2023-04-10 07:57:21 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Write your code here!// get the existing element with id "main"
+const main = document.querySelector('#main');
+
+// create a new h1 element
+const newHeader = document.createElement('h1');
+
+// set the id of the new h1 element to "victory"
+newHeader.id = "victory";
+
+// set the text content of the new h1 element to "YOUR-NAME is the champion"
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// replace the existing element with id "main" with the new h1 element
+main.replaceWith(newHeader);
